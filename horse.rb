e2fded9fafa0576dec908f8ac4aa1bf5297851ed
Deborah Hamel <deborahleehamel@gmail.com>
2015-12-21 12:27:33 -0700

    Create horse.rb

---
+++ horse.rb
@@ -0,0 +1,29 @@
+class Horse
+  def initialize
+    @galloping = false
+  end
+
+  def galloping?
+  end
+
+  def gallop
+  end
+
+  def rest
+  end
+end
+
+bessie = Horse.new
+anakin = Horse.new
+
+p bessie.galloping?
+p anakin.galloping?
+
+bessie.gallop
+p bessie.galloping?
+p anakin.galloping?
+
+bessie.rest
+anakin.gallop
+p bessie.galloping?
+p anakin.galloping?
